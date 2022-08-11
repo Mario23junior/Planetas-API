5134e4a0eed83cbccc5f5c254d9d8198f9c826f9
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-11 18:00:00 -0300

    Criando modelo de dados DTO das entidades

---
+++ src/main/java/com/api/planetas/dto/PlanetasDTO.java
@@ -0,0 +1,82 @@
+package com.api.planetas.dto;
+
+public class PlanetasDTO {
+
+	private Long id;
+	private String urlImage;
+	private String nome;
+	private String areaSuperficie;
+	private Double raio;
+	private Double distanciaDoSo;
+	private String duracaoDoDia;
+	private String descricao;
+
+	public PlanetasDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getUrlImage() {
+		return urlImage;
+	}
+
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getAreaSuperficie() {
+		return areaSuperficie;
+	}
+
+	public void setAreaSuperficie(String areaSuperficie) {
+		this.areaSuperficie = areaSuperficie;
+	}
+
+	public Double getRaio() {
+		return raio;
+	}
+
+	public void setRaio(Double raio) {
+		this.raio = raio;
+	}
+
+	public Double getDistanciaDoSo() {
+		return distanciaDoSo;
+	}
+
+	public void setDistanciaDoSo(Double distanciaDoSo) {
+		this.distanciaDoSo = distanciaDoSo;
+	}
+
+	public String getDuracaoDoDia() {
+		return duracaoDoDia;
+	}
+
+	public void setDuracaoDoDia(String duracaoDoDia) {
+		this.duracaoDoDia = duracaoDoDia;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+}

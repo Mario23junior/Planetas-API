1f8287cbb7731623dc47f70426393eb5ef4af149
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-11 18:01:05 -0300

    Model DTO de caracteristicas fiscais

--- src/main/java/com/api/planetas/dto/CaracteristicasFisicasDTO.java
+++ src/main/java/com/api/planetas/dto/CaracteristicasFisicasDTO.java
@@ -16,6 +16,16 @@ public class CaracteristicasFisicasDTO {
 
 	public Long getId() {
 		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getDiametroEquatorial() {
+		return diametroEquatorial;
+	}
+
 	public void setDiametroEquatorial(String diametroEquatorial) {
 		this.diametroEquatorial = diametroEquatorial;
 	}

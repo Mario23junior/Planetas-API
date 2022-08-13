7ff66a518d6cafbaa24305b361bddddbd38c4968
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-13 03:30:45 -0300

    inserindo configuracao do model mapper

---
+++ src/main/java/com/api/planetas/config/ModelMapperConfig.java
@@ -0,0 +1,16 @@
+package com.api.planetas.config;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ModelMapperConfig {
+	
+	@Bean
+	public ModelMapper modelMapper() {
+		ModelMapper modelMapper = new ModelMapper();
+		return modelMapper;
+	}
+	
+}

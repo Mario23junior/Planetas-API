404cf27adc7861aaef9b07b28c1da4cd526b29a3
Mário Júnior <48899365+Mario23junior@users.noreply.github.com>
2022-08-15 03:07:21 -0300

    Listagem de todos os valores

---
+++ src/main/java/com/api/planetas/service/FindAlldDataService.java
@@ -0,0 +1,40 @@
+package com.api.planetas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.stereotype.Service;
+
+import com.api.planetas.dto.PlanetasDTO;
+import com.api.planetas.model.Planetas;
+import com.api.planetas.repository.PlanetasRepository;
+
+@Service
+public class FindAlldDataService {
+  
+	private PlanetasRepository repository;
+	private ModelMapper mapper;
+	
+	public FindAlldDataService(PlanetasRepository repository , ModelMapper mapper) {
+		this.repository = repository;
+		this.mapper = mapper;
+	}
+	
+	public List<PlanetasDTO> listAllByData() {
+		return ((List<Planetas>)
+				  repository
+				  .findAll())
+				  .stream()
+				  .map(this::ConvertEntityToDTO)
+				  .collect(Collectors.toList());
+	}
+	
+	public PlanetasDTO ConvertEntityToDTO(Planetas planetas) {
+		mapper.getConfiguration()
+		.setMatchingStrategy(MatchingStrategies.LOOSE);
+		PlanetasDTO planetDto = mapper.map(planetas, PlanetasDTO.class);
+		return planetDto;
+	}
+}
